8ca4498f4f16b1cfccf43563e483b340d827f026
FHegardt <39218412+FHegardt@users.noreply.github.com>
2021-11-10 15:17:05 +0100

    moving bar chart

--- extraplot_FH.py
+++ extraplot_FH.py
@@ -10,11 +10,16 @@ athlete_events_drop = athlete_events.drop_duplicates(subset=["Event", "Games", "
 athlete_events_per_year = athlete_events_drop.groupby(["NOC","Year"]).count()
 athlete_events_per_year =  athlete_events_per_year.reset_index()
 athlete_events_per_year["Medal"] = athlete_events_per_year["Medal"].cumsum()
-athlete_events_per_year = athlete_events_per_year.dropna()
+#athlete_events_per_year = athlete_events_per_year.dropna()
+athlete_events_per_year = athlete_events_per_year.sort_values(["Year"])
 
 
-fig = px.scatter(athlete_events_per_year, x = "Medal", y = "Medal", 
-    size = "Medal", color = "NOC", size_max = 70,
-    log_x = True, animation_frame = "Year", title="Gapminder",
-    range_x = [0, 5000], range_y = [1890,2020])
+fig = px.bar(athlete_events_per_year, x="Medal", y="NOC", color="NOC",
+  animation_frame="Year", range_x=[0,40000])
 fig.show()
+
+#fig = px.scatter(athlete_events_per_year, x = "Medal", y = "Medal", 
+    #size = "Medal", color = "NOC", size_max = 70,
+    #log_x = True, animation_frame = "Year", title="Gapminder",
+    #range_x = [0, 5000], range_y = [1890,2020])
+#fig.show()
